8d563b99fd136c62b45101df7a6a11bb3a6a9e60
Rubikcon <rubikconnexus@gmail.com>
2025-08-15 13:28:05 +0100

    Updated index.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { Buffer } from 'buffer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
+  resolve: {
+    alias: {
+      buffer: 'buffer',
+    },
+  },
   server: {
     host: true,
   },
@@ -16,5 +25,6 @@ export default defineConfig({
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
+    include: ['buffer'],
   },
 });
